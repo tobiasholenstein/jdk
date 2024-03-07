7ed46fcb3b3033b74895d56ec20a47b99ee82ffe
Tobias Holenstein <tobias.holenstein@oracle.com>
2024-03-07 10:42:51 +0100

    remove todo

--- src/utils/IdealGraphVisualizer/View/src/main/java/com/sun/hotspot/igv/view/DiagramScene.java
+++ src/utils/IdealGraphVisualizer/View/src/main/java/com/sun/hotspot/igv/view/DiagramScene.java
@@ -311,7 +311,7 @@ public class DiagramScene extends ObjectScene implements DoubleClickHandler {
             }
 
             // update figure widget location
-            if (fw.isVisible()) { // TODO needed?
+            if (fw.isVisible()) {
                 fw.setPreferredLocation(figure.getPosition());
             }
         }
