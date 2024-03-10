da0aeed08f429d3df841c5a40e4dcf7e551f6415
Tobias Holenstein <tobias.holenstein@oracle.com>
2024-03-10 17:49:43 +0100

    remove title

--- src/utils/IdealGraphVisualizer/FilterWindow/src/main/java/com/sun/hotspot/igv/filterwindow/CheckRenderer.java
+++ src/utils/IdealGraphVisualizer/FilterWindow/src/main/java/com/sun/hotspot/igv/filterwindow/CheckRenderer.java
@@ -1,26 +1,3 @@
-/*
- * Copyright (c) 2008, 2015, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- *
- */
 package com.sun.hotspot.igv.filterwindow;
 
 import java.awt.*;
@@ -30,16 +7,11 @@ import javax.swing.JCheckBox;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
-/**
- * @author Thomas Wuerthinger
- */
 public class CheckRenderer extends JCheckBox implements ListCellRenderer<Object> {
 
-    private JList<Object> list;
-    private Color startBackground;
+    private final Color startBackground;
 
     public CheckRenderer(final JList<Object> list) {
-        this.list = list;
         list.addMouseListener(
                 new MouseAdapter() {
 
@@ -65,20 +37,20 @@ public class CheckRenderer extends JCheckBox implements ListCellRenderer<Object>
     public Component getListCellRendererComponent(final JList<? extends Object> list, Object value, final int index, boolean isSelected, boolean cellHasFocus) {
         setText(value.toString());
         CheckNode node = ((CheckNodeListModel) list.getModel()).getCheckNodeAt(index);
-        this.setSelected(node.isSelected());
-        this.setEnabled(list.isEnabled());
+        setSelected(node.isSelected());
+        setEnabled(list.isEnabled());
 
         if (isSelected && list.hasFocus()) {
-            this.setBackground(list.getSelectionBackground());
-            this.setForeground(list.getSelectionForeground());
+            setBackground(list.getSelectionBackground());
+            setForeground(list.getSelectionForeground());
         } else if (isSelected) {
             assert !list.hasFocus();
-            this.setBackground(startBackground);
-            this.setForeground(list.getForeground());
+            setBackground(startBackground);
+            setForeground(list.getForeground());
 
         } else {
-            this.setBackground(list.getBackground());
-            this.setForeground(list.getForeground());
+            setBackground(list.getBackground());
+            setForeground(list.getForeground());
         }
         return this;
     }
